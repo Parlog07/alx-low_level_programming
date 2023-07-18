623d197cacf4ebf6a60dcf77928de268b40603c7
AmineAyoub <ayoubmogador2014@gmail.com>
2023-07-18 14:57:45 -0700

    Pubg

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers gien as parameters
+ * @a: int a the first integer
+ * @b: int b the second interger
+ * Return: (Success)
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
