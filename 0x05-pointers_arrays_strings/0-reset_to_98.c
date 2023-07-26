7b9ef6f32475a3624a9d929e73c4c2a7e6732260
AmineAyoub <ayoubmogador2014@gmail.com>
2023-07-25 17:53:02 -0700

    x0x

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
-
 /**
- * reset_to_98 - function that takes a pointer to an integer parameter
- * @n: integer pointer
- * Return: 0 Success
- */
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Owned By Bwave/Bright
+*  Return: 0 is success
+*/
 
 void reset_to_98(int *n)
 {
 	*n = 98;
-} 
+}
